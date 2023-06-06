b699dc22f4badd15f29f5ed752e5745df07566a2
Lee Shoshani <lital.shoshani@gmail.com>
2023-06-06 19:03:04 +0300

    Feature: add basic structure file of network

---
+++ src/network.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+
+class Layer:
+    def __init__(self, num_neurons):
+        pass
+
+
+class Network:
+    def __init__(self):
+        pass
+
+    def forward(self, x):
+        pass
+
+    def __call__(self, *args, **kwargs):
+        pass
+
+    def save_network(self):
+        pass
+
+    def load_network(self, filepath: str):
+        pass
