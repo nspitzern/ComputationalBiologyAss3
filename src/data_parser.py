5e7ece060fffded3fabcdd84e3d7a1918ae6d69c
Lee Shoshani <lital.shoshani@gmail.com>
2023-06-07 22:17:17 +0300

    Chore: add DataItem to represent samples and labels

--- src/data_parser.py
+++ src/data_parser.py
@@ -1,17 +1,22 @@
 from typing import List, Tuple
+from collections import namedtuple
 
 from numpy import fromstring, ndarray
 
+DataItem = namedtuple('item', ['sample', 'label'])
 
-def load_data_file(filepath: str) -> Tuple[List[ndarray], List[int]]:
-    samples, labels = [], []
+
+def load_data_file(filepath: str) -> List[DataItem]:
+    items = []
 
     with open(filepath, 'r') as f:
         lines = f.readlines()
 
     for line in lines:
         sample, label = line.strip().split()
-        samples.append(fromstring(sample,'u1') - ord('0'))
-        labels.append(int(label))
+        # samples.append(fromstring(sample,'u1') - ord('0'))
+        # labels.append(int(label))
 
-    return samples, labels
+        items.append(DataItem(fromstring(sample, 'u1') - ord('0'), int(label)))
+
+    return items
