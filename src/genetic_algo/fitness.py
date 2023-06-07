3180d99a71d2f229140009933c5a7df1a35b8784
shimoncohen <shimoncohen655@gmail.com>
2023-06-08 01:45:18 +0300

    chore: add fitness class

---
+++ src/genetic_algo/fitness.py
@@ -0,0 +1,20 @@
+from typing import List
+
+from src.dataset import Dataset
+from src.genetic_algo.sample import Sample
+
+
+def correctness_ratio(sample: Sample, dataset: Dataset) -> float:
+    """
+    Check the ratio of decoded words that appear in the corpus
+    :param dec: List of decoded words
+    :param corpus: List of Corpus words
+    :return: float: ratio
+    """
+    count = 0
+
+    for s, lable in dataset:
+        if sample(s) == lable:
+            count += 1
+
+    return count / len(dataset)
