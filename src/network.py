b4cd902c8a76b2da2fef5531440073c38f9864f1
Lee Shoshani <lital.shoshani@gmail.com>
2023-06-07 23:53:56 +0300

    Feature: add save network (currently just layers). Chore: Change layers and activations to be protected attributes.

--- src/network.py
+++ src/network.py
@@ -1,5 +1,6 @@
 from typing import List
 from abc import ABC
+import json
 
 import numpy as np
 
@@ -7,6 +8,10 @@ import numpy as np
 class ActivationFunction(ABC):
     def __call__(self, x: np.ndarray):
         raise NotImplemented
+
+    def to_JSON(self):
+        return json.dumps(self, default=lambda o: o.__dict__,
+                          sort_keys=True, indent=4)
 
 
 class ReLU(ActivationFunction):
@@ -35,35 +40,46 @@ class BatchNorm:
 
 class Layer:
     def __init__(self, input_dim, output_dim):
-        self.weights = np.random.random(size=(input_dim, output_dim))
+        self.weights: np.ndarray = np.random.random(size=(input_dim, output_dim))
 
     def __call__(self, x: np.ndarray) -> np.ndarray:
         return x @ self.weights
 
+    def to_JSON(self):
+        return self.weights.tolist()
+
 
 class Network:
-    def __init__(self, layers_dim: List[int], activation: ActivationFunction):
-        self.__layers = []
+    def __init__(self, layers_dim: List[int], activations: List[ActivationFunction]):
+        self._layers = []
+        self._activations = []
 
         for dim_idx in range(len(layers_dim) - 1):
             input_l_dim = layers_dim[dim_idx]
             output_l_dim = layers_dim[dim_idx + 1]
-            self.__layers.append(Layer(input_dim=input_l_dim, output_dim=output_l_dim))
-            self.__layers.append(activation())
+            self._layers.append(Layer(input_dim=input_l_dim, output_dim=output_l_dim))
 
-        self.__layers[-1] = Sigmoid()
+        for activation in activations:
+            self._activations.append(activation())
+
+        self._activations.append(Sigmoid())
 
     def forward(self, x: np.ndarray) -> np.ndarray:
-        for f in self.__layers:
+        for f, a in zip(self._layers, self._activations):
             x = f(x)
+            x = a(x)
 
         return x
 
     def __call__(self, x: np.ndarray) -> np.ndarray:
         return self.forward(x)
 
-    def save_network(self):
-        pass
+    def save_network(self, filepath: str):
+        with open(filepath, 'w', encoding='utf-8') as f:
+            json.dump({
+                'weight': [l.to_JSON() for l in self._layers],
+                'activation': [a.to_JSON() for a in self._activations]
+            }, f)
 
     def load_network(self, filepath: str):
         pass
