69b7b714aaea2a658ea00c7d86e05de9b0ec1582
shimoncohen <shimoncohen655@gmail.com>
2023-06-08 01:26:51 +0300

    chore(sample): make sample network editable

--- src/genetic_algo/sample.py
+++ src/genetic_algo/sample.py
@@ -1,12 +1,12 @@
 from random import random
 import numpy as np
 
-from src.network import DynamicNetwork
+from src.network import Network
 
 
 class Sample:
-    def __init__(self, network: DynamicNetwork, mutation_magnitude: float):
-        self.__network: DynamicNetwork = network
+    def __init__(self, network: Network, mutation_magnitude: float):
+        self.__network: Network = network
         self.__magnitude = mutation_magnitude
 
     def mutate(self, threshold: float):
@@ -14,3 +14,12 @@ class Sample:
             if random() > threshold:
                 x = np.random.uniform(low=-self.__magnitude, high=self.__magnitude, size=l.shape)
                 l.weights += x
+    
+    def __getitem__(self, item):
+        return self.__network[item]
+    
+    def __setitem__(self, key, value):
+        self.__network[key] = value
+    
+    def __len__(self):
+        return len(self.__network)
