3d682f724b19a85590b51ded8fa9825cb477a744
Lee Shoshani <lital.shoshani@gmail.com>
2023-06-08 11:44:57 +0300

    Feature: add split to train and test datasets

--- buildnet.py
+++ buildnet.py
@@ -1,10 +1,11 @@
-from src.dataset import Dataset
+from src.dataset import Dataset, split_train_test
 from src.genetic_algo.simulator import SimulationArgs, Simulator
 from src.genetic_algo.strategy import GeneticAlgorithmType
 
 
 if __name__ == '__main__':
     dataset: Dataset = Dataset('nn0.txt')
+    train_dataset, test_dataset = split_train_test(dataset, train_ratio=0.7)
     args: SimulationArgs = SimulationArgs(0.97, elite_percentile=0.9, mutation_percentage=0.2, mutation_threshold=0.5)
     simulator = Simulator(GeneticAlgorithmType.REGULAR, 100, dataset, args)
     simulator.run()
--- src/dataset.py
+++ src/dataset.py
@@ -1,11 +1,17 @@
-from random import shuffle
+from random import shuffle, sample
+from typing import List, Tuple
 
 from src.data_parser import load_data_file, DataItem
 
 
 class Dataset:
-    def __init__(self, data_filepath: str, should_shuffle: bool = False):
-        self.__items = load_data_file(data_filepath)
+    def __init__(self, data_filepath: str = '', data_items: List[DataItem] = None, should_shuffle: bool = False):
+        if data_items:
+            self.__items = data_items
+        elif data_filepath != '':
+            self.__items = load_data_file(data_filepath)
+        else:
+            raise ValueError('Dataset not provided with data to load')
         self.__should_shuffle = should_shuffle
 
     def __len__(self) -> int:
@@ -19,3 +25,14 @@ class Dataset:
 
     def shuffle(self):
         shuffle(self.__items)
+
+
+def split_train_test(items: Dataset, train_ratio: float = 0.7) -> Tuple[Dataset, Dataset]:
+    num_items = len(items)
+    train_idxs = sample(list(range(num_items)), int(train_ratio * num_items))
+    test_idxs = set(range(num_items)) - set(train_idxs)
+
+    train_items = [items[i] for i in train_idxs]
+    test_items = [items[i] for i in test_idxs]
+
+    return Dataset(data_items=train_items), Dataset(data_items=test_items)
