62825021cf2cb44bc9990d0ec894b5ced09bf909
shimoncohen <shimoncohen655@gmail.com>
2023-06-08 15:10:21 +0300

    chore: rename and add one point crossover

--- src/genetic_algo/evolver.py
+++ src/genetic_algo/evolver.py
@@ -4,18 +4,35 @@ from random import randint
 
 from src.genetic_algo.sample import Sample
 from src.genetic_algo.selector import Selector
-from src.network import Network
 
 
 class Evolver:
     @staticmethod
-    def one_point_crossover(samples: List[Sample], fitness_scores: List[float]) -> List[Sample]:
+    def single_crossover(samples: List[Sample], fitness_scores: List[float]) -> List[Sample]:
         # Choose 2 samples for crossover
         s1, s2 = Selector.choose_n_weighted_random(samples, fitness_scores, 2)
         i = randint(0, len(s1) - 1)
-
+        
         cross1, cross2 = copy.deepcopy(s1), copy.deepcopy(s1)
+
+        # Swap one layer
         layer_1, layer_2 = s1[i], s2[i]
         cross1[i], cross2[i] = layer_2, layer_1
+
+        return [cross1, cross2]
+    
+    @staticmethod
+    def one_point_crossover(samples: List[Sample], fitness_scores: List[float]) -> List[Sample]:
+        # Choose 2 samples for crossover
+        s1, s2 = Selector.choose_n_weighted_random(samples, fitness_scores, 2)
+        i = randint(1, len(s1) - 2)
+
+        cross1, cross2 = copy.deepcopy(s1), copy.deepcopy(s1)
+        
+        # Swap one part of the sample (multiple layers)
+        layer_1_1, layer_2_1 = s1[:i], s2[:i]
+        layer_1_2, layer_2_2 = s1[i:], s2[i:]
+        cross1[:i], cross2[:i] = layer_2_1, layer_1_1
+        cross1[i:], cross2[i:] = layer_2_2, layer_1_2
 
         return [cross1, cross2]
