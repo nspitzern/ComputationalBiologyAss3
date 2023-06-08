9feaa177a497992f117f3758cc20ac28e39518dd
shimoncohen <shimoncohen655@gmail.com>
2023-06-08 12:56:21 +0300

    chore: cleanup fitness func

--- src/genetic_algo/fitness.py
+++ src/genetic_algo/fitness.py
@@ -1,6 +1,7 @@
+import numpy as np
+
 from src.dataset import Dataset
 from src.genetic_algo.sample import Sample
-
 
 def correctness_ratio(sample: Sample, dataset: Dataset) -> float:
     """
@@ -9,10 +10,4 @@ def correctness_ratio(sample: Sample, dataset: Dataset) -> float:
     :param corpus: List of Corpus words
     :return: float: ratio
     """
-    count = 0
-
-    for s, lable in dataset:
-        if sample(s) == lable:
-            count += 1
-
-    return count / len(dataset)
+    return np.mean([1 if sample(s) == label else 0 for s, label in dataset])
